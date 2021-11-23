c42e82e9682a8b3ef60d9281d1eea6938c1de42e
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-11-23 09:04:25 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EksamensProjekt2021
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
