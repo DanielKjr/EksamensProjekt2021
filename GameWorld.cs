c84755004e47562f97b0c0ac63c74cb28b314cc7
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-11-29 09:35:14 +0100

    Fiksede noget den af en eller anden grund ikke ville gemme før..

--- GameWorld.cs
+++ GameWorld.cs
@@ -1,5 +1,4 @@
-﻿using EksamensProjekt2021.GameObjects;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
