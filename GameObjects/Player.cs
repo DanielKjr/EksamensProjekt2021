2b1682ea5140ff386cfc2df2fde33ec28ccff8bb
BrothT3 <BrothT3@gmail.com>
2021-11-29 20:59:38 +0100

    dette rolf. rolf lave player movement+animation. rolf stor skaft

--- GameObjects/Player.cs
+++ GameObjects/Player.cs
@@ -1,25 +1,142 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EksamensProjekt2021
 {
     class Player : GameObject
     {
+        private Vector2 position = new Vector2(500, 300);
+        private int speed = 350;
+        //en værdi vi bare kan ændre til at passe med hvor hurtigt vi vil ha ham. bliver brugt i udregninen af når han bevæger sig
+        private bool isMoving = false;
+        //forklaret hvor den er relevant
+        private Dir direction = Dir.Right;
+        // enum'en som vi lavede ude i gameworld
+        private MouseState mStateOld = Mouse.GetState();
+        //ska vi bruger senere til shoot-funktion. trust me boiis.
+
+        public SpriteAnimation anim;
+        public SpriteAnimation[] animations = new SpriteAnimation[4];
+        // forklaret hvor den er relevant
+
+
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+        //property sårn vi ban benytte os af den
+
+
+
+
+        public void setX(float newX)
+        {
+            position.X = newX;
+        }
+        public void setY(float newY)
+        {
+            position.Y = newY;
+        }
+        //er her fordi ellers så kommer der en error med den ikke kan finde ud af vector fordi det en data-type og ik en værdi
+
+
         public override void OnCollision(GameObject other)
         {
-           
+
         }
 
         public override void Shoot(Weapon weapon)
         {
-            
+
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+            KeyboardState kState = Keyboard.GetState();
+            //keyboard.getstate() ved statusen på vores keyboard (om vi har trykket på en knap eller givet slip for eksempel). kstate gemmer det til en variabel, sårn vi kan benytte os af den
+
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds; //delta-time fordi ellers så afhænger tiden af performance
+
+            isMoving = false; // relevant fordi vi kun vil kører igennem vores animationer når vi bevæger os, så han ikke løber på stedet
+
+            if (kState.IsKeyDown(Keys.D)) //IsKeyDown ser om en knap er trykket på vores kState. 
+            {
+                direction = Dir.Right; // ændre direction, sårn vi ka brugedet til at vælge hvilken animation vi vil bruge
+                //if (position.X < 1920) ska finde en anden måde at begrænse det på
+                    position.X += speed * dt; //sådan at den bevæger sig
+                    isMoving = true; //forklaret ovenover
+            }
+            if (kState.IsKeyDown(Keys.A))
+            {
+                direction = Dir.Left;
+                if (position.X > 0)
+                    position.X -= speed * dt;
+                isMoving = true;
+            }
+            if (kState.IsKeyDown(Keys.W))
+            {
+                direction = Dir.Up;
+                if (position.Y > 0)
+                    position.Y -= speed * dt;
+                isMoving = true;
+            }
+            if (kState.IsKeyDown(Keys.S))
+            {
+                direction = Dir.Down;
+                if (position.Y < 1080)
+                    position.Y += speed * dt;
+                isMoving = true;
+            }
+            //det burde ændre hvilken animation man bruger efter en prioritet der tilsvare hvornår man skrev den ind, men dette virker da fuck fuk fuck min fucking pik fuck det lort her fuck.
+            /*
+            if (isMoving)
+            {
+                switch (direction)
+                {
+                    case Dir.Right:
+                        anim = animations[0];
+                        break;
+                    case Dir.Left:
+                        anim = animations[1];
+                        break;
+                    case Dir.Up:
+                        anim = animations[2];
+                        break;
+                    case Dir.Down:
+                        anim = animations[3];
+                        break;
+                }
+
+            } 
+            unødvendig kode, fordi enums faktisk er brugbart after all. vi typecaster vores dir til en int, som vi ka bruge i vores array med vores retninger.
+            det nedenunder gør essentielt det samme, men ser kloger ud. der må ikke fuckes med rækkefølgen af værdierne der hvor vi laver vores enum i gameWorld
+            */
+            anim = animations[(int)direction];
+            anim.Position = new Vector2(position.X - 20, position.Y - 48); // ska ændres til at passe spriten
+
+            if (isMoving)
+            {
+                anim.Update(gameTime);
+            }
+            else
+            {
+                if (direction == Dir.Right || direction == Dir.Up || direction == Dir.Right)
+                {
+                    anim.setFrame(1);
+                }
+                if (direction == Dir.Up)
+                {
+                    anim.setFrame(2);
+                }
+                // dette gør at hvis man ik bevæger sig, så sætter den animation til at være den bestemte frame tilsvarende på retning's spritesheet. synes de her er de bedste som idle frames. kan diskuteres
+            }
+
         }
     }
 }
--- GameWorld.cs
+++ GameWorld.cs
@@ -6,6 +6,13 @@ using System.Collections.Generic;
 
 namespace EksamensProjekt2021
 {
+    enum Dir
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
     public class GameWorld : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -14,19 +21,29 @@ namespace EksamensProjekt2021
         private List<Enemy> enemies;
         private List<Projectile> projectiles;
 
-        private Player player;
+        Player player = new Player();
 
         private Texture2D cursor;
+
+        private Texture2D playerSprite;
+        private Texture2D trumpWalkRight;
+        private Texture2D trumpWalkLeft;
+        private Texture2D trumpWalkUp;
+        private Texture2D trumpWalkDown;
+        
+
         private Song music;
 
         private int[] map;
 
-
+        public static Vector2 screenSize;     
         public GameWorld() 
         {
             _graphics = new GraphicsDeviceManager(this); 
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            
+            screenSize = new Vector2(_graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         }
 
         public void AddObject(GameObject go)
@@ -43,22 +60,36 @@ namespace EksamensProjekt2021
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //_graphics.IsFullScreen = true;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            playerSprite = Content.Load<Texture2D>("SpritePlaceHolder2");
+            trumpWalkRight = Content.Load<Texture2D>("trumpWalkRight");
+            trumpWalkLeft = Content.Load<Texture2D>("trumpWalkLeft");
+            trumpWalkUp = Content.Load<Texture2D>("trumpWalkUp");
+            trumpWalkDown = Content.Load<Texture2D>("trumpWalkDown");
 
-            // TODO: use this.Content to load your game content here
+            player.animations[0] = new SpriteAnimation(trumpWalkRight, 6, 10); // SpriteAnimation(texture2D texture, int frames, int fps) forklaret hvad de gør i SpriteAnimation.cs
+            player.animations[1] = new SpriteAnimation(trumpWalkLeft, 6, 10);
+            player.animations[2] = new SpriteAnimation(trumpWalkUp, 6, 10);
+            player.animations[3] = new SpriteAnimation(trumpWalkDown, 6, 10);
+            //enum kan castes til int, så derfor kan vi bruge et array til at skife imellem dem. forklaret i player og hvor det relevant
+
+            player.anim = player.animations[0]; //ændre sig afhængig af direction i player
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
+            player.Update(gameTime);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -71,6 +102,11 @@ namespace EksamensProjekt2021
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+
+            _spriteBatch.Begin();
+            player.anim.Draw(_spriteBatch); //vi bruger Draw metoden i den SpriteAnimation "anim" som vi lavede på playeren. det ser fucking nice ud fordi det er så simpelt
+            _spriteBatch.End();
+
         }
 
         
