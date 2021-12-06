c8ae45850b3f908dddcef2594f8b129a164290d4
Roy42069360 <15274@student.eadania.dk>
2021-12-06 10:12:47 +0100

    hitscan shhoot

--- GameObjects/Hitscan.cs
+++ GameObjects/Hitscan.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,19 @@ namespace EksamensProjekt2021
 {
     class Hitscan : Weapon
     {
+
+        public static void Shoot()
+        {
+            MouseState mState = Mouse.GetState();
+            mState.Position.ToVector2();
+            Vector2 mPos = mState.Position.ToVector2();
+
+            Rectangle rectangle;
+            rectangle.X = (int)mPos.X;
+            rectangle.Y = (int)mPos.Y;
+            rectangle.Height = 1;
+            rectangle.Width = 1;
+            
+        }
     }
 }
