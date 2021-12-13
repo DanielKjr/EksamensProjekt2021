5ce3313f7a9cffef7ee0ce862c2119f2521a7b9c
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-12-13 09:13:53 +0100

    Removed redundant fields and used properties instead.

--- GameObjects/Weapons/Hitscan/AK47.cs
+++ GameObjects/Weapons/Hitscan/AK47.cs
@@ -5,14 +5,12 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace EksamensProjekt2021
 {
-    class AK47 : Hitscan
+    class AK47 : Hitscan 
     {
         
         public AK47()
         {
 
-            
-            Name = "AK47";
 
             range = 700;
             damage = 5;
@@ -24,7 +22,7 @@ namespace EksamensProjekt2021
         {
             sprite = content.Load<Texture2D>("Weapons/AK-47");
 
-            Sprite = sprite;
+
 
             origin = new Vector2(this.sprite.Width / 2, this.sprite.Height / 2);
             gunFire = content.Load<SoundEffect>("SoundEffects/SingleShot");
